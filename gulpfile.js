271c05054c43ae34f0a7fc48a7149e9cfe80a540
Andrey Dovydenko <dovydenko.andrej@gmail.com>
2017-03-02 00:27:40 +0700

    never gonna minify anything here

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp  = require('gulp'),
-    gutil = require('gulp-util'),
     concat = require('gulp-concat'),
     htmlmin = require('gulp-htmlmin'),
     jshint = require('gulp-jshint'),
@@ -7,8 +6,6 @@ var gulp  = require('gulp'),
 
 gulp.task('process-html', function() {
     return gulp.src('./html/*.html')
-        //only uglify if gulp is ran with '--type production'
-        .pipe(gutil.env.type === 'prod' ?  htmlmin({collapseWhitespace: true}) : gutil.noop())
         .pipe(gulp.dest('./output'));
 });
 
@@ -17,8 +14,6 @@ gulp.task('process-js', function() {
         .pipe(jshint())
         .pipe(jshint.reporter(stylish))
         .pipe(concat('script.js'))
-        //only uglify if gulp is ran with '--type production'
-        .pipe(gutil.env.type === 'prod' ? uglify() : gutil.noop())
         .pipe(gulp.dest('./output'));
 });
 
