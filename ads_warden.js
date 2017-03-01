f6b0fcbf19e16cc52934f21f96cd594ce39d6a05
Andrey Dovydenko <dovydenko.andrej@gmail.com>
2017-03-01 19:06:15 +0700

    multi rules easy exapanble arch

--- ads_warden.js
+++ ads_warden.js
@@ -17,7 +17,7 @@ function RectHelper(){
 }
 RectHelper.prototype = {
     /*
-        gets the ratio of inner_rect inside outer_rect
+        gets the ratio of inner_rect inside outer_rect by area
      */
     getOverlappingRatio: function(inner_rect, outer_rect){
         var intersection = this.getIntersection(inner_rect, outer_rect);
@@ -43,6 +43,9 @@ RectHelper.prototype = {
         return (rect.width === 0 || rect.height === 0);
     },
 
+    /*
+        Gets an intersection rect of two rect or an empty rect if there is no intersection
+     */
     getIntersection: function (first ,second) {
         if (this.isEmpty(first) || this.isEmpty(second))
             return this.makeZeroBasedRect(0, 0);
@@ -62,31 +65,95 @@ RectHelper.prototype = {
     }
 };
 
+function IAdVisibilityRule(){
+
+}
+IAdVisibilityRule.prototype = {
+    checkVisibility: function(id, ruleConfig){
+        throw {name : "NotImplemented", message : "Can't call the interface"};
+    }
+};
+
+function InViewportRule() {
+    IAdVisibilityRule.call(this);
+    this.rectHelper = new RectHelper();
+}
+InViewportRule.prototype = new IAdVisibilityRule();
+InViewportRule.prototype.checkVisibility = function(id, ruleConfig){
+    var ad = document.getElementById(id);
+    var adRect = ad.getBoundingClientRect();
+    var viewportRect = this.rectHelper.makeZeroBasedRect(window.innerHeight, window.innerWidth);
+
+    var overlapping = this.rectHelper.getOverlappingRatio(adRect, viewportRect);
+    return {
+        visible: ruleConfig.threshold ? overlapping >= ruleConfig.threshold : overlapping===1.0,
+        overlapping: overlapping
+    }
+};
+
+function DocumentFocusedRule() {
+    IAdVisibilityRule.call(this);
+}
+DocumentFocusedRule.prototype = new IAdVisibilityRule();
+DocumentFocusedRule.prototype.checkVisibility = function(id, ruleConfig){
+    return {
+        visible: document.hasFocus()
+    }
+};
+
+
 /*
     Get and sanitize data for advert position and viewport size, using RectHelper to detect visibility
  */
 function AdsWarden(){
+    /*
+        Those are rulse we have to determine is ad visible or not,
+        we could easily increase amount of rules by adding them here
+     */
+    this.rules = {
+        'inside_viewport': new InViewportRule(),
+        'document_focused': new DocumentFocusedRule()
+        };
 }
 
 AdsWarden.prototype = {
-
     /*
-        checks if advert is 100% visible
+        accumulating results of multiple checks
      */
-    isVisible: function(id, rules) {
-        return this.getVisibility(id, rules) === 100.0;
+    combineResults: function(currentResult, newResult, rule_name ){
+        if (!newResult.visible)
+            currentResult.violatedRules.push(rule_name);
+
+        currentResult.visible = currentResult.visible && newResult.visible;
+
+        for (var name in newResult) {
+            if (newResult.hasOwnProperty(name) && name !== 'visible') {
+                currentResult[name] = newResult[name];
+            }
+        }
+        return currentResult;
     },
-
     /*
-        returns a float value for visibility percentage
+        Getting the rule by its name and executing it against current config and advert id
      */
-    getVisibility: function(id, rules) {
-        var ad = document.getElementById(id);
-        var rect_helper = new RectHelper();
-        var ad_rect = ad.getBoundingClientRect();
-        var viewport_rect = rect_helper.makeZeroBasedRect(window.innerHeight, window.innerWidth);
+    checkSingleRule: function(id, ruleConfig, ruleName){
+        if(this.rules[ruleName]){
+            return this.rules[ruleName].checkVisibility(id, ruleConfig)
+        }
+    },
+    /*
+        Checks all the rules provided and accumulate results
+        if any rule rendered advert as invisibile return value would have result.visible === false
+     */
+    getVisibility: function(id, rulesConfig) {
 
-        var overlapping = rect_helper.getOverlappingRatio(ad_rect, viewport_rect);
-        console.log(overlapping*100, ad_rect, viewport_rect);
+        // if none of rules violated considering ad visible
+        var result = {visible: true, violatedRules:[]};
+        for (var ruleName in rulesConfig) {
+            if (rulesConfig.hasOwnProperty(ruleName)) {
+                result = this.combineResults(result, this.checkSingleRule(id, rulesConfig[ruleName], ruleName), ruleName)
+            }
+        }
+        return result;
     }
 };
--- script.js
+++ script.js
@@ -3,9 +3,24 @@
  *                              VARIABLES DECLARATION                                           *
  *                                                                                              *
  ************************************************************************************************/
-var adIsViewable = true,
-    viewabilityTime = 0,
-    adElement = document.getElementById("ad");
+/*
+    Setting up the Warden object to keep an eye on our advert
+ */
+var warden = new AdsWarden();
+var warden_config = {
+    'inside_viewport':{threshold: 0.75},
+    'document_focused':{}
+};
+
+/*
+    Helper function for pretty descriptive string
+ */
+function getDescriptiveString(result) {
+    var str = result.visible ? 'The ad is viewable ' : 'The ad is NOT viewable ';
+    if(result.violatedRules.length > 0)
+        str += 'Violated rules are: ' + result.violatedRules.join(',');
+    return str;
+}
 
 /**
  * Logs the viewability values in the console
@@ -13,9 +28,9 @@ var adIsViewable = true,
  * @override
  */
 window.log = function () {
-    var warden = new AdsWarden();
-    warden.getVisibility('ad');
-    console.log("Ad is viewable: ", adIsViewable, "\nViewability time of the ad in sec:", viewabilityTime);
+    var result = warden.getVisibility('ad', warden_config);
+    console.log(getDescriptiveString(result));
+    //console.log("Ad is viewable: ", adIsViewable, "\nViewability time of the ad in sec:", viewabilityTime);
 };
 
 /************************************************************************************************
